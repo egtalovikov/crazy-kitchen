9525e901cd41f75c167a693a7736e283a990f5dd
Egor Talovikov <egtalovikov@gmail.com>
2023-08-16 15:04:54 +0500

    feat: добавил тест

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,4 +1,14 @@
+import App from './App'
+import { render, screen } from '@testing-library/react'
+
+const appContent = 'MAIN PAGE'
+
 // @ts-ignore
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
 )
+
+test('Example test', async () => {
+  render(<App />)
+  expect(screen.getByText(appContent)).toBeDefined()
+})
