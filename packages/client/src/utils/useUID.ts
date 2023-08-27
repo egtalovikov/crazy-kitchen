6af2ef331034b839cbfb03ffe67d0c13c3e11ff6
Evgenia <epol1394@gmail.com>
2023-08-27 13:59:29 +0300

    ck-20: Сверстать страницу лидерборда

---
+++ packages/client/src/utils/useUID.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+export const generateUID = (): string => {
+  return Math.random().toString(36).substring(2) + Date.now().toString(36)
+}
+
+export const useUID = () => {
+  const [uid, setUID] = useState('')
+
+  useEffect(() => {
+    const generatedUID = generateUID()
+    setUID(generatedUID)
+  }, [])
+  return uid
+}
