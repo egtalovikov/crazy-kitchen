f995c9828bf2d633127688e1585f451c99d1c226
Evgenia1394 <78655029+Evgenia1394@users.noreply.github.com>
2023-09-07 21:14:52 +0300

    Сверстать экрÐck-21: °н с началом игры (#10)

--- packages/client/src/pages/StartPage/index.tsx
+++ packages/client/src/pages/StartPage/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from './StartPage.module.scss'
+import { MAIN_ROUTE } from '../../utils/consts'
+import { useGoToRoute } from '../../utils/useGoToRoute'
+import { ButtonBlue } from '../../components/Button'
 
 const StartPage = () => {
-  return <>start</>
+  const { goRoute } = useGoToRoute()
+
+  return (
+    <div className={styles.background}>
+      <ButtonBlue onClickCallback={() => goRoute(MAIN_ROUTE)}>start</ButtonBlue>
+    </div>
+  )
 }
 
 export default StartPage
