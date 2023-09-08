016af1db0e13feb45374758851a49be97ecca741
Aleksandr Malykh <80761063+malykhs1@users.noreply.github.com>
2023-09-08 21:22:58 +0300

    change base url

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from 'axios'
 
 const $host = axios.create({
-  baseURL: 'https://ya-praktikum.tech/api/v2',
+  baseURL: __BASE_URL__,
 })
 
 const authInterceptor = (config: InternalAxiosRequestConfig) => {
