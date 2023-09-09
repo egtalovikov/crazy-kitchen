60d636f83832c325633fb213e9dafcc3e325d05a
Egor Talovikov <egtalovikov@gmail.com>
2023-09-09 18:19:03 +0300

    hotfix: убрал vite-plugin-pwa

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
-import { VitePWA } from 'vite-plugin-pwa'
 dotenv.config()
 
 // https://vitejs.dev/config/
-// @ts-ignore
-// @ts-ignore
 export default defineConfig({
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
@@ -15,21 +12,5 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
     __BASE_URL__: `"${process.env.BASE_URL}"`,
   },
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        name: 'Crazy Kitchen',
-        short_name: 'CK',
-        description: '',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-      },
-      workbox: {
-        // @ts-ignore
-        swSrc: 'sw.ts',
-        swDest: 'dist/service-worker.js',
-      },
-    }),
-  ],
+  plugins: [react()],
 })
