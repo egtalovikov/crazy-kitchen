76ffbac95bf8692693f0e6f9f34234785e365846
Hellga2023 <helllga2018@yandex.ru>
2023-09-25 14:54:22 +0400

    build issue fixed

--- packages/client/src/game/parameters/ingredientsOnBurgerParams.ts
+++ packages/client/src/game/parameters/ingredientsOnBurgerParams.ts
@@ -10,21 +10,15 @@ const ingredientsOnBurgerParams = {
     spriteY: 208,
     spriteWidth: 220,
     spriteHeight: 90,
-    // frameWidth: 100,
-    //startPoint: { x: 1335, y: 750 },
-    //preparationRequired: false,
   },
   [Ingredients.Cutlet]: {
-    width: 220,
-    height: 90,
+    width: 120,
+    height: 50,
     imageSrc: set,
     spriteX: 365,
     spriteY: 100,
     spriteWidth: 220,
     spriteHeight: 90,
-    // frameWidth: 100,
-    //startPoint: { x: 1200, y: 750 },
-    //preparationRequired: true,
   },
   [Ingredients.Tomato]: {
     width: 120,
@@ -34,9 +28,6 @@ const ingredientsOnBurgerParams = {
     spriteY: 100,
     spriteWidth: 220,
     spriteHeight: 90,
-    // frameWidth: 100,
-    //startPoint: { x: 1200, y: 650 },
-    //preparationRequired: false,
   },
   [Ingredients.Bread]: {
     width: 120,
@@ -46,19 +37,7 @@ const ingredientsOnBurgerParams = {
     spriteY: 520,
     spriteWidth: 240,
     spriteHeight: 100,
-    // frameWidth: 100,
-    //startPoint: { x: 800, y: 730 },
-    //preparationRequired: false,
   },
-  // todo bread top
-  /* [Ingredients.Bread]: {
-    width: 220,
-    height: 90,
-    imageSrc: set,
-    frameWidth: 100,
-    startPoint: { x: 800, y: 730 },
-    preparationRequired: false,
-  }, */
 }
 
 export default ingredientsOnBurgerParams
--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,38 +1,5 @@
-import React, { useEffect, useMemo } from 'react'
-import GameCanvas from '../../Modules/GameDraw'
-import { setOrder } from '../../utils/gameCore'
-import totalBurgerImage from '../../assets/ingredients/totalBurger.png'
-import burgerTomatoSaladImage from '../../assets/ingredients/burgerTomatoSalad.png'
-import burgerCheeseSaladImage from '../../assets/ingredients/burgerCheeseSalad.png'
 import Game from './Game'
-
-const GameDraw = () => {
-  const { indexOrder } = setOrder()
-
-  const imgSrcOrder = useMemo(() => {
-    switch (indexOrder) {
-      case 0:
-        return totalBurgerImage as string
-      case 1:
-        return burgerTomatoSaladImage as string
-      case 2:
-        return burgerCheeseSaladImage as string
-      default:
-        return ''
-    }
-  }, [indexOrder])
-
-  useEffect(() => {
-    // @ts-ignore
-    new GameCanvas({})
-  }, [])
-
-  // @ts-ignore
-  return <GameCanvas orderImage={imgSrcOrder} />
-}
 
 export const GameWithEngine = () => {
   return <Game />
 }
-
-export default GameDraw
