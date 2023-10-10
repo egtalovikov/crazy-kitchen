73c74a0013b68aeff029870c4215b40ab6cce67e
Egor Talovikov <egtalovikov@gmail.com>
2023-10-10 21:06:39 +0300

    feat: добавил AppRouter вместо App

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import AppRouter from './components/AppRouter'
 import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 import { store } from './store'
 import { Provider } from 'react-redux'
+import CssBaseline from '@mui/material/CssBaseline'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <ErrorBoundary>
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <CssBaseline />
+          <AppRouter />
+        </BrowserRouter>
       </Provider>
     </ErrorBoundary>
   </React.StrictMode>
