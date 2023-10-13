d379c672a7563b0fc78cf01fb16b0956e8edcb5f
Hellga2023 <helllga2018@yandex.ru>
2023-10-13 19:00:45 +0400

    feature/ck-46 routs added

--- packages/server/api/api-router.ts
+++ packages/server/api/api-router.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import topicController from '../controllers/topic.controller'
+import themeController from '../controllers/themeController'
 
 const router = Router()
 router.post('/topics', topicController.createTopic)
+router.post('/theme', themeController.changeUserTheme)
+router.get('/theme', themeController.getUserTheme)
 
 const apiRouter = router
 export default apiRouter
